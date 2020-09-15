8690079d740e9093dbcde0b6defecf023520db6a
Alexey Vasilyev <alexey.vasilyev@gmail.com>
2020-09-15 18:23:53 +0300

    Logout service

--- src/app/services/index.ts
+++ src/app/services/index.ts
@@ -1,4 +1,5 @@
 export * from './camlist.service';
 export * from './eventlist.service';
 export * from './login.service';
+export * from './logout.service';
 export * from './windowref.service';
---
+++ src/app/services/logout.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Server, ServerResponse } from '../models'
+
+@Injectable()
+export class LogoutService {
+
+    constructor(private http: HttpClient) {
+    }
+
+    getLogout(server: Server, token: String): Promise<any> {
+        const url = `${server.server_addr}/api/v1/logout?token=${token}`;
+        return this.http
+            .get<ServerResponse>(url)
+            .toPromise()
+            .then()
+            .catch(this.handleError);
+        //{"data":{"token", "abc123456789def"}}
+    }
+
+    private handleError(error: HttpErrorResponse): Promise<any> {
+        // console.error('An error occurred', error);
+        return Promise.reject(error);
+    }
+
+}
