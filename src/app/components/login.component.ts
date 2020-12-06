c8abe9f96a07456d0aca190f4f8b2b07eab695f3
Alexey Vasilyev <alexey.vasilyev@gmail.com>
2020-12-06 15:26:09 +0200

    video.js update

--- src/app/components/login.component.ts
+++ src/app/components/login.component.ts
@@ -73,13 +73,9 @@ import StorageUtils from '../utils-storage';
         </mat-card-content>
       </mat-card>
 
-      <div style="padding: 20px;" class="app-text-right">
-        Use <a href="/old/">Old</a> web UI
-      </div>
-
       <div style="padding-top: 20px; padding-bottom: 10px" class="mat-small app-text-center">
         Want to have constant 24/7 recording? Check  <a href="https://cloud.tinycammonitor.com/">tinyCam Cloud</a> service.<br/>
-        <a href="https://tinysolutionsllc.com/">Tiny Solutions LLC., 2010–2020</a>
+        <a href="https://tinysolutionsllc.com/">Tiny Solutions LLC., 2010–2021</a>
       </div>
     </div>
   `,
